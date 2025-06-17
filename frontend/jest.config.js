b02a2b4b7d93b88f6bdc11bf38e566a24aa405c6
miura <miura@CORP.FINEGATE.CO.JP>
2025-06-17 10:27:23 +0900

    CIの設定

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -7,4 +7,13 @@ export default {
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 80,
+      functions: 100,
+      lines: 90
+    }
+  }
 };
